f5d52242c7c2feb734a23110583add9cefc117c4
andeechu <andychuabsurd@gmail.com>
2020-11-16 18:35:47 -0800

    added js functionality to display one svg at a time

--- scripts.js
+++ scripts.js
@@ -1,5 +1,19 @@
+
+let svg_ids = ['frog_svg', 'cookie_svg']
+
 function hide_svg(el_id) {
-    const frog = document.getElementById(el_id)
-    const d = frog.style.display
-    frog.style.display = d == '' || d == 'none' ? 'inline' : 'none'
+    const svg = document.getElementById(el_id)
+    const d = svg.style.display
+    svg.style.display = d == '' || d == 'none' ? 'inline' : 'none'
+    if (svg.style.display !== 'none') {
+        hide_others(el_id)
+    }
+}
+
+function hide_others(el_id) {
+    for (const svg of svg_ids){
+        if ( el_id !== svg ) {
+            document.getElementById(svg).style.display = 'none'
+        }
+    }
 }
