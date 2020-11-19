5fbb211537b8f0aa9ed9208663b80898508e48a0
andeechu <andychuabsurd@gmail.com>
2020-11-18 21:20:28 -0800

    cookie animation will now stay within div

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ let svg_ids = ['frog_svg', 'cookie_svg']
 function hide_svg(el_id) {
     const svg = document.getElementById(el_id)
     const d = svg.style.display
+    console.log(d)
     svg.style.display = d == '' || d == 'none' ? 'inline' : 'none'
     if (svg.style.display !== 'none') {
         hide_others(el_id)
